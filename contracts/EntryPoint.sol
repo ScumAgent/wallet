357033f8fb32a0a2fbd945bd5c9aafbf2f23986a
solidity12 <volibear1001@gmail.com>
2025-02-09 20:05:47 +0900

    feat: add EntryPointMockup contract

---
+++ contracts/EntryPoint.sol
@@ -0,0 +1,6 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.28;
+
+import {EntryPoint} from "@account-abstraction/contracts/core/EntryPoint.sol";
+
+contract EntryPointMockup is EntryPoint {}
